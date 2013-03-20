3d2732d35bc8b621597a804b0ccbdba7d11dc593
hurle1s <Shawn.Hurley21@gmail.com>
2013-03-20 12:43:00 -0400

    THis is the pliminary addition of Player will work on dealer next

--- Card.py
+++ Card.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python
 
 class Card():
 	#face value is the number that the card will use zero will be the intial
@@ -13,4 +13,30 @@ class Card():
 		self.name = name
 		self.face_down = face_down
 
-	def 
+
+	def get_face_value(self):
+		"""Get the face value"""
+		return self.face_value
+
+
+	def get_name(self):
+		"""Get the name of the card"""
+		return self.name
+
+
+	def set_face_value(self, face_value):
+		"""Set the face value to the given value"""
+		self.face_value = face_value
+
+	def set_face_down(self, face_down):
+		"""Set the face down Boolean to specified Value"""
+		self.face_down = face_down
+
+
+	def show_card(self):
+		if (self.face_down):
+			print "Face Down\n"
+		else:
+			print "*****Card****\nName: %s\nFace Value:  %d\n" % (self.name, self.face_value)
+
+
---
+++ Player.py
@@ -0,0 +1,45 @@
+from Hand import Hand
+class Player():
+	"""Player will represent the behavior and data of a player"""
+	#These will be the class variables.
+	hand = Hand()
+	money = 0
+	bet = 0
+
+	def __init__(self, money):
+		self.money = money
+
+
+	def get_hand(self):
+		"""Return the entire hand"""
+		return self.hand
+
+	def get_value_of_hand(self):
+		"""Return the value of the hand"""
+		return self.hand.get_value_of_hand()
+
+	def get_number_of_cards(self):
+		"""Return the number of cards"""
+		return self.hand.get_num_of_cards()
+
+	def set_bet(self, bet):
+		"""Set the bet for this player"""
+		self.bet = bet
+
+	def get_bet(self):
+		"""Get the bet for this player"""
+		return self.bet
+
+	def get_money(self):
+		"""Get the Money for this player"""
+		return self.money
+
+	def recieve_new_card(self, card):
+		"""Should take the new Card and add it to the hand"""
+		self.hand.add_card(card)
+
+	def want_card(self):
+		"""Ask this player if they would like to get another card"""
+		choice = raw_input("Would you like another card? (Please Enter True or Fals")
+		return bool(choice)
+		
