126cc2154a9fa0fa54e568d576deb808c741e168
seanmead <seanmead21@gmail.com>
2013-04-04 10:08:14 -0300

    Seems cleaner this way

--- Deck.py
+++ Deck.py
@@ -1,73 +1,26 @@
-from Card import Card
 from random import shuffle
-
+from Card import Card
 
 class Deck():
-    """Initialize new self.deck and shuffle"""
-    #self.deck is list which represents the self.deck of cards
-    #number of cards initialized to 0. Will be equal to length of self.deck list
-    __num_of_cards = 0
 
     def __init__(self):
-        self.deck = []
-        self.deck.append(Card(2, "2 of Spades", False))
-        self.deck.append(Card(2, "2 of Hearts", False))
-        self.deck.append(Card(2, "2 of Diamonds", False))
-        self.deck.append(Card(2, "2 of Clubs", False))
-        self.deck.append(Card(3, "3 of Spades", False))
-        self.deck.append(Card(3, "3 of Hearts", False))
-        self.deck.append(Card(3, "3 of Diamonds", False))
-        self.deck.append(Card(3, "3 of Clubs", False))
-        self.deck.append(Card(4, "4 of Spades", False))
-        self.deck.append(Card(4, "4 of Hearts", False))
-        self.deck.append(Card(4, "4 of Diamonds", False))
-        self.deck.append(Card(4, "4 of Clubs", False))
-        self.deck.append(Card(5, "5 of Spades", False))
-        self.deck.append(Card(5, "5 of Hearts", False))
-        self.deck.append(Card(5, "5 of Diamonds", False))
-        self.deck.append(Card(5, "5 of Clubs", False))
-        self.deck.append(Card(6, "6 of Spades", False))
-        self.deck.append(Card(6, "6 of Hearts", False))
-        self.deck.append(Card(6, "6 of Diamonds", False))
-        self.deck.append(Card(6, "6 of Clubs", False))
-        self.deck.append(Card(7, "7 of Spades", False))
-        self.deck.append(Card(7, "7 of Hearts", False))
-        self.deck.append(Card(7, "7 of Diamonds", False))
-        self.deck.append(Card(7, "7 of Clubs", False))
-        self.deck.append(Card(8, "8 of Spades", False))
-        self.deck.append(Card(8, "8 of Hearts", False))
-        self.deck.append(Card(8, "8 of Diamonds", False))
-        self.deck.append(Card(8, "8 of Clubs", False))
-        self.deck.append(Card(9, "9 of Spades", False))
-        self.deck.append(Card(9, "9 of Hearts", False))
-        self.deck.append(Card(9, "9 of Diamonds", False))
-        self.deck.append(Card(9, "9 of Clubs", False))
-        self.deck.append(Card(10, "10 of Spades", False))
-        self.deck.append(Card(10, "10 of Hearts", False))
-        self.deck.append(Card(10, "10 of Diamonds", False))
-        self.deck.append(Card(10, "10 of Clubs", False))
-        self.deck.append(Card(10, "Jack of Spades", False))
-        self.deck.append(Card(10, "Jack of Hearts", False))
-        self.deck.append(Card(10, "Jack of Diamonds", False))
-        self.deck.append(Card(10, "Jack of Clubs", False))
-        self.deck.append(Card(10, "Queen of Spades", False))
-        self.deck.append(Card(10, "Queen of Hearts", False))
-        self.deck.append(Card(10, "Queen of Diamonds", False))
-        self.deck.append(Card(10, "Queen of Clubs", False))
-        self.deck.append(Card(10, "King of Spades", False))
-        self.deck.append(Card(10, "King of Hearts", False))
-        self.deck.append(Card(10, "King of Diamonds", False))
-        self.deck.append(Card(10, "King of Clubs", False))
-        self.deck.append(Card(11, "Ace of Spades", False))
-        self.deck.append(Card(11, "Ace of Hearts", False))
-        self.deck.append(Card(11, "Ace of Diamonds", False))
-        self.deck.append(Card(11, "Ace of Clubs", False))
+    values = [2,3,4,5,6,7,8,9,10]
+	suits = ["Spades","Hearts","Diamonds","Clubs"]
+	faces = ["Jack of ","Queen of ","King of "]
+	ace = "Ace of "
+	of = " of "
+	self.deck = []
+	for suit in suits:
+    	    for value in values:
+		self.deck.append(Card(value,(str(value)+of+suit),False))
+    	    for face in faces:
+		self.deck.append(Card(10,(face+suit),False))
+    	    self.deck.append(Card(11,(ace+suit),False))
 
-        shuffle(self.deck)
-        self.__num_of_cards = len(self.deck)
+	shuffle(self.deck)
+    self.num_cards = len(self.deck)
 
-    def deal_card(self):
-        """Give a card off top of self.deck"""
+    def get_card(self):
         card = self.deck.pop()
-        self.__num_of_cards = len(self.deck)
+        self.num_cards-=1
         return card
